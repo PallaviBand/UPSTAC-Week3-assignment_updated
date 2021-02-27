030c9c8d66e8a9f70426fe639e1918c8ec40420f
PallaviBand <pallaviband2@gmail.com>
2021-02-28 01:15:14 +0530

    Initial commit

---
+++ src/main/java/org/upgrad/upstac/UpstacApplication.java
@@ -0,0 +1,25 @@
+package org.upgrad.upstac;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
+
+import java.io.File;
+
+@SpringBootApplication
+@Log4j2
+public class UpstacApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(UpstacApplication.class, args);
+//		String filename= System.getProperty("user.home")+ File.separator + "upstacApp.pid";
+//		SpringApplication application=new SpringApplication(UpstacApplication.class);
+//		log.info("Updated Process Id is available on" + filename);
+//		application.addListeners(new ApplicationPidFileWriter(filename));
+//		application.run();
+	}
+
+
+}
